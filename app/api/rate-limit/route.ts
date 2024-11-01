9d90f111c3719204a421c3ae52bd5648e51c5349
prince <prince.oncada@gmail.com>
2024-11-01 06:58:55 -0400

    feature: rate limiting api

--- app/api/rate-limit/route.ts
+++ app/api/rate-limit/route.ts
@@ -11,5 +11,23 @@ export const runtime = "edge";
 
 export async function GET(req: NextRequest) {
     const ip = (req.headers.get('x-forwarded-for') ?? '127.0.0.1').split(',')[0]
-    return NextResponse.json({ ip })
+    const { limit, reset, remaining } = await ratelimit.limit(ip)
+
+    if (remaining === 0) {
+        return NextResponse.json({
+            error: 'Rate limit exceeded',
+            limit,
+            reset,
+            remaining
+        }, {
+            status: 429,
+            headers: {
+                'X-RateLimit-Limit': limit.toString(),
+                'X-RateLimit-Remaining': remaining.toString(),
+                'X-RateLimit-Reset': reset.toString()
+            }
+        })
+    } else {
+        return NextResponse.json({ message: "Request Valid" }) // empty response
+    }
 }
