26a3fcae6e54db9018b33ae446ce33cee1cb49f0
Prince Oncada <prince.oncada@email.com>
2024-11-01 10:33:26 -0400

    dev: added loading, added comments, finalized working form with honeypot, recaptcha, and rate limit features.

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -10,6 +10,8 @@ import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { Textarea } from "./ui/textarea";
 import { useGoogleReCaptcha } from "react-google-recaptcha-v3";
+import { useToast } from "@/hooks/use-toast"
+import { LoaderCircle } from "lucide-react";
 
 const formSchema = z.object({
     emailAddress: z.string().email(),
@@ -22,19 +24,24 @@ const formSchema = z.object({
 export default function ContactForm() {
     const [honeyPotFieldName, setHoneyPotFieldName] = useState<string>("honeypot");
     const [loadTime, setLoadTime] = useState<number>(Date.now());
+    const [loading, setLoading] = useState<boolean>(false);
+    const { toast } = useToast()
 
+    // Generate a random field name for the honeypot field
     useEffect(() => {
         const randomFieldName = `field_${Math.random().toString(36).substring(7)}`;
         setHoneyPotFieldName(randomFieldName);
         setLoadTime(Date.now());
     }, [])
 
+    // Dynamically add the honeypot field to the form schema
     const dynamicFormSchema = formSchema.extend({
         [honeyPotFieldName]: z.string().optional().refine((value) => {
             return value === "" && Date.now() - loadTime > 3000;
         }, { message: "Spam detected!" })
     });
 
+    // Initialize the form with the dynamic schema
     const form = useForm<z.infer<typeof dynamicFormSchema>>({
         resolver: zodResolver(dynamicFormSchema),
         defaultValues: {
@@ -46,20 +53,29 @@ export default function ContactForm() {
         },
     })
 
+    // Register the honeypot field into the form schema
     useEffect(() => {
         form.register(honeyPotFieldName);
     }, [honeyPotFieldName, form])
 
+    // Initialize Google ReCaptcha Hook for Form Submission Verification
     const { executeRecaptcha } = useGoogleReCaptcha();
 
+    // Handler for Form Submission
     async function handleSubmit(values: z.infer<typeof dynamicFormSchema>) {
+        // Set Loading State to True
+        setLoading(true);
+
+        // Verify Recaptcha Token
         if (!executeRecaptcha) {
             console.error("Recaptcha not initialized");
             return;
         }
 
+        // Initialize Recaptcha Token
         const gRecaptchaToken = await executeRecaptcha("contact_form");
 
+        // Submit Recaptcha Token to Server and Verify Response based on Score
         const response = await axios({
             method: "POST",
             url: "/api/recaptcha-submit",
@@ -71,12 +87,57 @@ export default function ContactForm() {
             }
         });
 
+        // Check if Recaptcha Verification was Successful
         if (response.data.success === true) {
-            console.log("Recaptcha verification successful");
-            console.log(values);
-        } else {
-            console.error("Recaptcha verification failed");
+            // Successful Recaptcha Verification
+            // Finalize Email HTML Template Body 
+
+            // Initialize User Feedback Variables
+            let title: string; 
+            let description: string;
+
+            // Request to Send Email
+            await axios({
+                method: "POST",
+                url: "/api/send-email",
+                data: {
+                    from: values.emailAddress,
+                    subject: values.subject,
+                    body: values.message
+                },
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }).then((response) => {
+
+                // Successful Email Send User Feedback with Status Code 200
+                title = response.status === 200 ? "Email Sent!" : "Something went wrong.";
+                description = response.status === 200 ? "Your email was sent successfully." : "Failed to send email.";
+
+                // Reset Form Fields on Successful Email Send
+                form.reset();
+
+            }).catch((error) => {
+                
+                // Uncaptured errors response status codes responses
+                title = "Something went wrong.";
+                description = "Contact form is currently unavailable.";
+
+                // Rate Limit Exceeded User Feedback with Status Code 429
+                title = error.status === 429 ? "Rate Limit Exceeded" : title;
+                description = error.status === 429 ? "Please wait a moment before sending another email." : description;
+            }).finally(() => {
+
+                // Display User Feedback Toast Notification
+                toast({
+                    title,
+                    description
+                })
+            })
         }
+
+        // Set Loading State to False
+        setLoading(false);
     }
 
     return (
@@ -141,8 +202,8 @@ export default function ContactForm() {
                         </FormItem>
                     }}></FormField>
 
-                    <Button type="submit" className="w-full">
-                        Submit
+                    <Button type="submit" className="w-full" disabled={loading}>
+                        {loading ? <LoaderCircle className="animate-spin" size={24} /> : "Submit"}
                     </Button>
                 </form>
             </Form>
